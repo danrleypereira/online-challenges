60fa28d64a496ddb810d55c099fe2690163c09f5
danrley willyan <danrleywillian@gmail.com>
2015-08-16 12:45:55 -0300

    Change the 1018URI exercise, now it is working correctly, but URI judge isn't accepting this.

--- 1018/1018URI.c
+++ 1018/1018URI.c
@@ -1,50 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int counter(int y);
-double HT;
+int valueInsert, counter(int y);
+int notas[7] = {100, 50, 20, 10, 5, 2, 1};
+
 int main(){
+	int i;
+
 	do{
-		scanf("%lf", &HT);
-	}while(HT > 1000000 || HT < 0);
+		scanf("%d", &valueInsert);
+	}while(valueInsert > 1000000 || valueInsert < 0);
 	
-	printf("%.0lf\n", HT);
-	
-	counter(100);
-	printf("HT = %.4lf\n", HT);
-	counter(50);
-	printf("HT = %.4lf\n", HT);
-	counter(20);
-	printf("HT = %.4lf\n", HT);
-	counter(10);
-	printf("HT = %.4lf\n", HT);
-	counter(5);
-	printf("HT = %.4lf\n", HT);
-	counter(2);
-	printf("HT = %.4lf\n", HT);
-	counter(1);
-	printf("HT = %.4lf\n", HT);
-	
-	return 0;	
-}
-counter(int y){
-	double numberLF, decpart;
-	int numberInt;
-	
-	numberLF = HT/y;
-	printf("numberLF = %lf", numberLF);
-	numberInt = (int)numberLF;
-	printf("numberInt = %d", numberInt);
-	decpart = numberLF - numberInt;
-	printf("decpart = %lf", decpart);
-	if(numberLF != numberInt){
-		HT = decpart * y;
-		printf("%d nota(s) de R$ %d,00\n", numberInt, y);
-	}
-	else{
-		printf("%lf nota(s) de R$ %.2d\n PORRA\n", numberLF, y);
-		HT = 0;
+	for(i = 0; i < 7; i++){
+		printf("%d nota(s) de R$ %d,00 \n", counter(notas[i]), notas[i]);
 	}
 	
 	return 0;
+}
+
+counter(valueNota){
+	int value = valueInsert/valueNota;	
+	valueInsert = valueInsert % valueNota;
+
+	return value;
 }
