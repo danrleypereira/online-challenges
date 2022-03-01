c39420a5beec64390d9bfe7520ab2f898b3ec43d
danrleypereira <danrleywillian@gmail.com>
2022-03-01 07:51:35 -0300

    HashSet simple solution using buckets

--- leetcode/hashtable/hash_set.py
+++ leetcode/hashtable/hash_set.py
@@ -19,6 +19,24 @@ class MyHashSet:
             return True
         return False
 
+class MyHashSetWithBuckets:
+
+    def __init__(self):
+        self.hash = [[] for _ in range(1000)]
+        
+    def add(self, key: int) -> None:
+        if not self.contains(key):
+            val = key%1000
+            self.hash[val].append(key)
+
+    def remove(self, key: int) -> None:
+        if self.contains(key):
+            val = key%1000
+            self.hash[val].remove(key)
+
+    def contains(self, key: int) -> bool:
+        val = key%1000
+        return key in self.hash[val]
 
 # Your MyHashSet object will be instantiated and called as such:
 # obj = MyHashSet()
