3ce0a8406501042f9962fa2cc4e59c4774bf86ee
Danrley Pereira <danrleywillian@gmail.com>
2023-10-01 14:25:00 -0300

    TicTacToe - Remains to implement ColumnWinner, AlreadyTakenField and PlayerInteraction

---
+++ katas/TicTacToe/TicTacToe/Board.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+
+namespace TicTacToe
+{
+    public class Board
+    {
+        private char[,] board;
+        private int numberOfTurns = 0;
+        public Board()
+        {
+            board = new char[3,3];
+        }
+        public void NextPlayerChose(int row, int column)
+        {
+            this.board[row, column] = numberOfTurns++%2 == 0 ? 'X' : 'O';
+        }
+        public char GetPosition(int row, int column)
+        {
+            return this.board[row, column];
+        }
+        public bool IsThereARowWinner()
+        {
+            for(int i = 0; i < 3; i++)
+            {
+                bool rowTakenBy = true;
+                rowTakenBy = rowTakenBy && board[i, 0] == board[i, 1];
+                rowTakenBy = rowTakenBy && board[i, 0] == board[i, 2];
+                if(rowTakenBy) return true;
+            }
+            return false;
+        }
+        public bool IsThereADiagonalWinner()
+        {
+            if( board[0, 0] == board[1, 1] && board[0, 0] == board[2, 2] )
+                return true;
+            if(board[0, 2] == board[1, 1] && board[1, 1] == board[2, 0])
+                return true;
+            return false;
+        }
+        public bool isGameOver()
+        {
+            bool remainsFieldsEmpty = true;
+            for(int i = 0; i < 3; i++)
+                for(int j = 0; j < 3; j++)
+                    remainsFieldsEmpty = remainsFieldsEmpty && board[i, j] == '\0';
+            return !remainsFieldsEmpty;
+        }
+    }
+}
---
+++ katas/TicTacToe/TicTacToeTest/BoardShould.cs
@@ -0,0 +1,108 @@
+using Microsoft.VisualBasic.FileIO;
+using TicTacToe;
+
+namespace TicTacToeTest;
+
+public class BoardShould
+{
+    public static IEnumerable<object[]> RowWinnersData()
+    {
+        var board1 = new Board();
+        board1.NextPlayerChose(0, 0);
+        board1.NextPlayerChose(1, 0);
+        board1.NextPlayerChose(0, 1);
+        board1.NextPlayerChose(1, 1);
+        board1.NextPlayerChose(0, 2);
+        yield return new object[] { board1 };
+
+        var board2 = new Board();
+        board2.NextPlayerChose(1, 0);
+        board2.NextPlayerChose(1, 1);
+        board2.NextPlayerChose(1, 2);
+        yield return new object[] { board2 };
+
+        var board3 = new Board();
+        board3.NextPlayerChose(2, 0);
+        board3.NextPlayerChose(2, 1);
+        board3.NextPlayerChose(2, 2);
+        yield return new object[] { board3 };
+    }
+
+    public static IEnumerable<object[]> NoWinnersData()
+    {
+        var board1 = new Board();
+        board1.NextPlayerChose(0, 0); // X
+        board1.NextPlayerChose(0, 1); // O
+        board1.NextPlayerChose(0, 2); // X
+        board1.NextPlayerChose(1, 0); // O
+        board1.NextPlayerChose(1, 1); // X
+        board1.NextPlayerChose(1, 2); // O
+        board1.NextPlayerChose(2, 0); // X
+        board1.NextPlayerChose(2, 1); // O
+        board1.NextPlayerChose(2, 2); // X
+        yield return new object[] { board1 }; // Full board, no winner
+
+        var board2 = new Board();
+        board2.NextPlayerChose(0, 0); // X
+        board2.NextPlayerChose(0, 1); // O
+        board2.NextPlayerChose(0, 2); // X
+        board2.NextPlayerChose(1, 0); // O
+        board2.NextPlayerChose(1, 1); // X
+        board2.NextPlayerChose(1, 2); // O
+        board2.NextPlayerChose(2, 0); // X
+        board2.NextPlayerChose(2, 2); // O
+        yield return new object[] { board2 }; // Not full board, no winner
+    }
+
+    public static IEnumerable<object[]> DiagonalWinnersData()
+    {
+        var board1 = new Board();
+        board1.NextPlayerChose(0, 0); // X
+        board1.NextPlayerChose(0, 1); // O
+        board1.NextPlayerChose(1, 1); // X
+        board1.NextPlayerChose(0, 2); // O
+        board1.NextPlayerChose(2, 2); // X (Winner with diagonal from top-left to bottom-right)
+        yield return new object[] { board1 };
+
+        var board2 = new Board();
+        board2.NextPlayerChose(0, 2); // X
+        board2.NextPlayerChose(0, 0); // O
+        board2.NextPlayerChose(1, 1); // X
+        board2.NextPlayerChose(1, 0); // O
+        board2.NextPlayerChose(2, 0); // X
+        board2.NextPlayerChose(2, 2); // O (Winner with diagonal from top-right to bottom-left)
+        yield return new object[] { board2 };
+    }
+
+
+    [Fact]
+    public void InsertionOnEmptyFieldTest()
+    {
+        Board board = new Board();
+        board.NextPlayerChose(1, 2);
+        Assert.Equal('X', board.GetPosition(1, 2));
+        board.NextPlayerChose(2, 2);
+        Assert.Equal('O', board.GetPosition(2, 2));
+        board.NextPlayerChose(0, 2);
+        Assert.Equal('X', board.GetPosition(0, 2));
+    }
+    [Theory]
+    [MemberData(nameof(RowWinnersData))]
+    public void RowsAreTakenByTheSamePlayer(Board board)
+    {
+        Assert.True(board.IsThereARowWinner());
+    }
+    [Theory]
+    [MemberData(nameof(NoWinnersData))]
+    public void GameOverWithoutWinner(Board board)
+    {
+        Assert.True(board.isGameOver());
+    }
+    [Theory]
+    [MemberData(nameof(DiagonalWinnersData))]
+    public void DiagonalWins(Board board)
+    {
+        Assert.True(board.IsThereADiagonalWinner());
+    }
+
+}
